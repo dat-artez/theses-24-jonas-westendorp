0ed68d3ae2fc8eedbcd7c52db72f26dd7c99fa4b
Jonas <tuberjonas@gmail.com>
2024-04-07 02:11:04 +0200

    created draggable windows

---
+++ src/lib/window.svelte
@@ -0,0 +1,78 @@
+<script lang="ts">
+	// props
+	export let title: String;
+	export let focus;
+
+	// state
+	let windowElement: HTMLDivElement;
+	let isDragging: boolean = false;
+	let pos = { x: 5, y: 5 };
+	let mouseOffsetBar: [number, number] = [0, 0];
+
+	function handleClose() {
+		title = title + 'a';
+	}
+
+	function mouseDown(e: MouseEvent) {
+		e.stopPropagation();
+		e.preventDefault();
+
+		console.log('mouse on ' + title);
+
+		isDragging = true;
+		let rect = windowElement.getBoundingClientRect();
+		let { x, y } = rect;
+		let { clientX, clientY } = e;
+
+		mouseOffsetBar = [clientX - x, clientY - y];
+
+		focus();
+	}
+
+	function handleMouseMove(e: MouseEvent) {
+		if (!isDragging) return;
+
+		let { clientX, clientY } = e;
+		let [xOffset, yOffset] = mouseOffsetBar;
+
+		pos.x = clientX - xOffset;
+		pos.y = clientY - yOffset;
+	}
+</script>
+
+<div
+	bind:this={windowElement}
+	class="draggable-container"
+	style={`top:${pos.y}px;left:${pos.x}px;`}
+>
+	<div class="window">
+		<div
+			class="title-bar"
+			on:mousedown={mouseDown}
+			on:mouseup={(e) => (isDragging = false)}
+			on:mouseout={(e) => (isDragging = false)}
+			on:mousemove={handleMouseMove}
+		>
+			<button aria-label="Close" class="close" on:click={handleClose}></button>
+			<h1 class="title">{title}</h1>
+			<button aria-label="Resize" class="resize"></button>
+		</div>
+		<div class="separator"></div>
+
+		<div class="window-pane">Hello world!</div>
+		{#if isDragging}
+			dragging
+		{:else}
+			not dragging
+		{/if}
+	</div>
+</div>
+
+<style>
+	.draggable-container {
+		position: absolute;
+	}
+	.title-bar:hover {
+		cursor: grab;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+	import Window from '$lib/window.svelte';
+
+	let windows = [{ title: '0. Preface' }, { title: '1. Introduction' }];
+
+	function focusWindow(title: string) {
+		// let windowIndex = null;
+		// let window = windows.find((x, i) => {
+		// 	if (x.title === title) {
+		// 		windowIndex = i;
+		// 		return x.title === title;
+		// 	}
+		// });
+
+		let focusWindow = null;
+		//remove the focuswindow
+		let newWindows = windows.filter((win) => {
+			if (win.title === title) {
+				focusWindow = win;
+				return false;
+			} else return true;
+		});
+		// add it back at the end
+		windows = [...newWindows, focusWindow];
+	}
+</script>
+
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		<title>Document</title>
+		<link rel="stylesheet" href="https://unpkg.com/@sakun/system.css" />
+	</head>
+
+	<body>
+		{#each windows as window (window.title)}
+			<Window title={window.title} focus={() => focusWindow(window.title)} />
+		{/each}
+		<!-- <Window title={'1. introduction'} focus={() => focusWindow('1. introduction')} /> -->
+	</body>
+</html>
+
+<style>
+	body {
+		width: 100vw;
+		height: 100vh;
+	}
+</style>
