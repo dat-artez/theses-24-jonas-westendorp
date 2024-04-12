55828f6bf446e60be76c2e72f806c98b2cad2cd6
Jonas <tuberjonas@gmail.com>
2024-04-12 17:51:58 +0200

    added window resize

--- src/lib/window.svelte
+++ src/lib/window.svelte
@@ -9,15 +9,13 @@
 	// state
 	let windowElement: HTMLDivElement;
 	let isDragging: boolean = false;
-	let pos = { x: 5, y: 5 };
+	let window = { x: 5, y: 5, width: 400, height: 500 };
 	let mouseOffsetBar: [number, number] = [0, 0];
 
 	// handlers
 	function handleMouseDown(e: MouseEvent) {
 		e.stopPropagation();
 		e.preventDefault();
-
-		console.log('mouse on ' + title);
 
 		isDragging = true;
 		let rect = windowElement.getBoundingClientRect();
@@ -28,36 +26,59 @@
 
 		focus();
 
+		return { mouse: [clientX, clientY], offset: [clientX - x, clientY - y] };
+	}
+
+	function handleMouseMove(callback) {
+		if (!isDragging) return;
+
 		// listen for the other mouse events; and remove listeners once dragging has stopped
-		document.body.addEventListener('mousemove', handleMouseMove);
+		document.body.addEventListener('mousemove', callback);
 		document.body.addEventListener(
 			'mouseup',
 			(e) => {
 				isDragging = false;
-				document.body.removeEventListener('mousemove', handleMouseMove);
+				document.body.removeEventListener('mousemove', callback);
 			},
 			{ once: true }
 		);
 	}
 
-	function handleMouseMove(e: MouseEvent) {
-		if (!isDragging) return;
+	function moveWindow(e: MouseEvent) {
+		mouseOffsetBar = handleMouseDown(e).offset;
 
-		let { clientX, clientY } = e;
 		let [xOffset, yOffset] = mouseOffsetBar;
 
-		pos.x = clientX - xOffset;
-		pos.y = clientY - yOffset;
+		function move(e) {
+			window.x = e.clientX - xOffset;
+			window.y = e.clientY - yOffset;
+		}
+
+		handleMouseMove(move);
+	}
+
+	function resizeWindow(e: MouseEvent) {
+		mouseOffsetBar = handleMouseDown(e).offset;
+		let [xOffset, yOffset] = mouseOffsetBar;
+
+		let rect = windowElement.getBoundingClientRect();
+
+		function move(e) {
+			window.width = e.clientX - rect.x;
+			window.height = e.clientY - rect.y;
+		}
+
+		handleMouseMove(move);
 	}
 </script>
 
 <div
 	bind:this={windowElement}
 	class="draggable-container"
-	style={`top:${pos.y}px;left:${pos.x}px;`}
+	style={`top:${window.y}px;left:${window.x}px;width:${window.width}px;height:${window.height}px;`}
 >
 	<div class="window" on:click={focus}>
-		<div class="title-bar" on:mousedown={handleMouseDown}>
+		<div class="title-bar" on:mousedown={moveWindow}>
 			<button aria-label="Close" class="close" on:click={close}></button>
 			<h1 class="title">{title}</h1>
 			<button aria-label="Resize" class="resize"></button>
@@ -68,18 +89,38 @@
 			<Markdown source={myMarkdown} />
 		</div>
 	</div>
+	<div class="scale-corner" on:mousedown={resizeWindow}>x</div>
 </div>
 
 <style>
 	.draggable-container {
 		position: absolute;
 	}
+	.window {
+		width: 100%;
+		height: 100%;
+		margin: 0;
+		display: grid;
+		grid-template-rows: auto 1rem;
+	}
 	.title-bar:hover {
 		cursor: grab;
 	}
 
+	.scale-corner {
+		width: 10px;
+		height: 10px;
+		background-color: red;
+		position: absolute;
+		right: 0;
+		bottom: 0;
+		cursor: se-resize;
+	}
+
 	.window-pane {
-		max-height: 90vh;
+		max-height: 80vh;
+		max-width: 80vw;
+		box-sizing: border-box;
 	}
 
 	:global(h1) {
